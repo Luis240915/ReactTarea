c4b2733b2ee36f2d9fe26b5a0bfad32fbd55eea7
Luis240915 <30506035+Luis240915@users.noreply.github.com>
2024-11-12 10:37:46 -0600

    Create MiComponente.js
    Actualización de Componente

---
+++ MiComponente.js
@@ -0,0 +1,23 @@
+import React from "react";
+
+const MiComponente =() =>{
+    const nombre= "Luis";
+    const direccion= "ciudad";
+    let usuario ={
+        nombre: "Jose Ramírez",
+        direccion: "6av. 0-15 zona 2"
+    }
+    return(
+        <di>
+            <h1>Información</h1>
+            <ul>
+                <li>Nombre: <strong>{nombre}</strong></li>
+            </ul>
+            
+        </di>
+        
+    )
+        
+}
+
+export default MiComponente;
